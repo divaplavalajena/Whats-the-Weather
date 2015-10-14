c93f2f4ebd30ed7e5e7cbc1fd8ccd4f8a63097e1
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-14 14:49:18 -0500

    Added code for URL and url handling also changes to plist file to accept non https pages

--- Whats the Weather/ViewController.swift
+++ Whats the Weather/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/Paris/forecasts/latest")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                print(webContent)
+                
+            }
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
