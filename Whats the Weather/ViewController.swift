770f24a36eb134689e14c71ff21d2e9fd0d8a37b
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-14 15:42:38 -0500

    moved website code into button handler

--- Whats the Weather/ViewController.swift
+++ Whats the Weather/ViewController.swift
@@ -16,15 +16,6 @@ class ViewController: UIViewController {
     
     @IBAction func findWeather(sender: AnyObject) {
         
-        
-        
-    }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         let url = NSURL(string: "http://www.weather-forecast.com/locations/Paris/forecasts/latest")!
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
@@ -44,7 +35,7 @@ class ViewController: UIViewController {
                     //print(weatherArray[0])
                     
                     if weatherArray.count > 0 {
-                    
+                        
                         let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
                         
                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -67,6 +58,13 @@ class ViewController: UIViewController {
         }
         
         task.resume()
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
 
